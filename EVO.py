2fe1722361d867bc5718de9601a0f6b805139e86
ancestor <tomvandecraats@gmail.com>
2018-06-01 18:34:20 +0200

    Added some formatting, and display the generated world

--- EVO.py
+++ EVO.py
@@ -1,12 +1,22 @@
 from pprint import pprint
 from modules import entity
-# from modules import world
+from modules import world
 
 
 def main():
+    print("=" * 30)
+    print("= Peon test - Generate 1 peon, print vars")
+    print("=" * 30)
     peon = entity.Entity()
     pprint(vars(peon))
     pprint(peon.vitals.vitals)
+
+    print("=" * 30)
+    print("= World generation test - generate a small sample world, print vars")
+    print("=" * 30)
+
+    world_map = world.WorldMap(height = 5, width = 5, depth = 2)
+    pprint(vars(world_map))
 
 if __name__ == "__main__":
     main()
